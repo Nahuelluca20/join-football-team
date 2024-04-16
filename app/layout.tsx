5916909e93691ca7a8c02721bb19d714ec0ea704
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-16 20:04:26 -0300

    add more ui components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {Inter as FontSans} from "next/font/google";
 
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import {TooltipProvider} from "@/components/ui/tooltip";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
       </body>
     </html>
   );
