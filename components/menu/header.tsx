82cf04e94c3d4eda00501a939c4716240df3b067
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-18 09:38:11 -0300

    Refactor menu/header component, add dynamic breadcrumbs based on pathname

--- components/menu/header.tsx
+++ components/menu/header.tsx
@@ -46,6 +46,8 @@ export default function Header() {
 
   const pathname = usePathname();
 
+  const splitPathname = pathname.split("/");
+
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <Sheet>
@@ -86,23 +88,35 @@ export default function Header() {
       </Sheet>
       <Breadcrumb className="hidden md:flex">
         <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="#">Dashboard</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="#">Orders</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-          </BreadcrumbItem>
+          {splitPathname.map((path, index) => {
+            return path === "" ? (
+              ""
+            ) : (
+              <>
+                <BreadcrumbItem key={`breadcrum-${index}`}>
+                  {splitPathname.length === index + 1 ? (
+                    <BreadcrumbPage>
+                      <BreadcrumbLink asChild>
+                        <Link href={path === "dashboard" ? `/${path}` : `${path}`}>
+                          {path.replace(/^\w/, (c) => c.toUpperCase())}
+                        </Link>
+                      </BreadcrumbLink>
+                    </BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink asChild>
+                      <Link href={path === "dashboard" ? `/${path}` : `${path}`}>
+                        {path.replace(/^\w/, (c) => c.toUpperCase())}
+                      </Link>
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+                {splitPathname.length !== index + 1 && <BreadcrumbSeparator />}
+              </>
+            );
+          })}
         </BreadcrumbList>
       </Breadcrumb>
+
       <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
