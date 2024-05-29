52a6ffa001376246c14dfa9b51dd1e367882159a
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-29 09:14:01 -0300

    chore: Update page layout and add Go to App button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
 import SessionButton from "@/components/buttons/session-button";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={"/dashboard"}>Dashboard</Link>
-      <SessionButton />
+    <main className="flex min-h-screen flex-col items-center space-y-5 p-24">
+      <h1 className="text-2xl md:text-3xl font-bold">Proyecto: Falta uno</h1>
+      <div className="flex gap-3 items-center">
+        <Button asChild>
+          <Link href={"/dashboard"}>Go to App</Link>
+        </Button>
+        <SessionButton />
+      </div>
     </main>
   );
 }
